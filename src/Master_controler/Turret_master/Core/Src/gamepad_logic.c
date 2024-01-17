f7e4dde2226ae374d69396dc2f7adc4e89d1d9ac
Luka Bradarić Lisić <luka.bradariclisic@gmail.com>
2024-01-17 12:56:20 +0100

    Added more error redundancy

--- src/Master_controler/Turret_master/Core/Src/gamepad_logic.c
+++ src/Master_controler/Turret_master/Core/Src/gamepad_logic.c
@@ -16,23 +16,22 @@ volatile uint8_t gamepad_ready = 0;
 void gamepad_procesByte(uint8_t *byte){
 	switch(recieving_state){
 		case 0:
-			if(*byte == GAMEPAD_DATA)
+			if(*byte == GAMEPAD_DATA && !gamepad_ready)
 				recieving_state++;
 			break;
 
 		case 1:
 			if(*byte == GAMEPAD_END_DATA){
-				recieving_state++;
-				gamepad_ready = 1;
+				if(buffer_ind == 28){
+					gamepad_ready = 1;
+				}
+
+				recieving_state = 0;
 				buffer_ind = 0;
+
 			} else {
 				gamepad_input.byteArray[buffer_ind++] = *byte;
 			}
-			break;
-
-		case 2:
-			if(!gamepad_ready)
-				recieving_state = 0;
 			break;
 	}
 }
