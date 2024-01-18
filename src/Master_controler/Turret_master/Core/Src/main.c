7b2d58bf30c6cc710e1da9d1ecef653938d3a05e
Eugen Bosnjak <eugen.bosnjak@fer.hr>
2024-01-18 15:49:23 +0100

    Fixed stepper and added servo input

--- src/Master_controler/Turret_master/Core/Src/main.c
+++ src/Master_controler/Turret_master/Core/Src/main.c
@@ -42,6 +42,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
 TIM_HandleTypeDef htim2;
 
 UART_HandleTypeDef huart2;
@@ -57,6 +58,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_TIM1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -71,21 +73,14 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 	}
 }
 
-uint32_t convertToStepperPeriod(int32_t controllerValue) {
-	if (controllerValue == 0) {
-		return 0;
-	}
+uint32_t convertToServoPWM(int32_t controllerValue) {
+	float leftY = 3125.0f;
+	float rightY = 6250.0f;
 
-	if (controllerValue < 0) {
-		controllerValue *= -1;
-	}
+	float leftX = 512.0f;
+	float rightX = -512.0f;
 
-	// controller = 1   ==> CCR = 62500 (leftY)
-	// controller = 512 ==> CCR = 12500  (rightY)
-	float leftY = 62500.0f;
-	float rightY = 12500.0f;
-
-	float y = leftY + ((controllerValue - 1) / 512.0f) * (rightY - leftY);
+	float y = (controllerValue - leftX) / (rightX - leftX) * (rightY - leftY) + leftY;
 
 	return (uint32_t) y;
 }
@@ -121,17 +116,27 @@ int main(void) {
 	MX_GPIO_Init();
 	MX_USART2_UART_Init();
 	MX_TIM2_Init();
+	MX_TIM1_Init();
 	/* USER CODE BEGIN 2 */
 
 	HAL_UART_Receive_IT(&huart2, &rx_data, 1);
 
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
+	htim1.Instance->PSC = 7;
+	htim1.Instance->ARR = 62500;
+	htim1.Instance->CCR1 = 4627;
+	htim1.Instance->EGR |= 0x01;
+
 	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
 
-	htim2.Instance->ARR = 25000;
-	htim2.Instance->CCR1 = 7500;
+	htim2.Instance->ARR = 28500;
+	htim2.Instance->CCR1 = 25;
 	htim2.Instance->EGR |= 0x01;
 
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+
+	uint32_t oldServoPWM = htim1.Instance->CCR1;
 
 	/* USER CODE END 2 */
 
@@ -147,21 +152,23 @@ int main(void) {
 
 		if (x > 0) {
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
-		}
-		else {
+		} else if (x < 0) {
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
 		}
 
-
-		uint32_t stepperPeriod = convertToStepperPeriod(x);
-
-		if (stepperPeriod == 0 && htim2.Instance->CCR1 != 0) {
+		if (x == 0 && htim2.Instance->CCR1 != 0) {
 			htim2.Instance->CCR1 = 0;
 			htim2.Instance->EGR |= 0x01;
-		} else if (stepperPeriod > 0 && htim2.Instance->CCR1 == 0) {
-			htim2.Instance->CCR1 = 7500;
-			htim2.Instance->ARR = 25000; //stepperPeriod;
+		} else if (x != 0 && htim2.Instance->CCR1 == 0) {
+			htim2.Instance->CCR1 = 25;
+			htim2.Instance->ARR = 28500;
 			htim2.Instance->EGR |= 0x01;
+		}
+
+		uint32_t servoPWM = convertToServoPWM(y);
+		if (servoPWM != oldServoPWM) {
+			htim1.Instance->CCR1 = servoPWM;
+			oldServoPWM = servoPWM;
 		}
 
 		gamepad_enableNewData();
@@ -214,6 +221,77 @@ void SystemClock_Config(void) {
 	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
 		Error_Handler();
 	}
+}
+
+/**
+ * @brief TIM1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_TIM1_Init(void) {
+
+	/* USER CODE BEGIN TIM1_Init 0 */
+
+	/* USER CODE END TIM1_Init 0 */
+
+	TIM_ClockConfigTypeDef sClockSourceConfig = { 0 };
+	TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+	TIM_OC_InitTypeDef sConfigOC = { 0 };
+	TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = { 0 };
+
+	/* USER CODE BEGIN TIM1_Init 1 */
+
+	/* USER CODE END TIM1_Init 1 */
+	htim1.Instance = TIM1;
+	htim1.Init.Prescaler = 0;
+	htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim1.Init.Period = 65535;
+	htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	htim1.Init.RepetitionCounter = 0;
+	htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+	if (HAL_TIM_Base_Init(&htim1) != HAL_OK) {
+		Error_Handler();
+	}
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_TIM_PWM_Init(&htim1) != HAL_OK) {
+		Error_Handler();
+	}
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+	sConfigOC.OCMode = TIM_OCMODE_PWM1;
+	sConfigOC.Pulse = 0;
+	sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+	sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+	sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+	sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+	sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+	if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+	sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+	sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+	sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+	sBreakDeadTimeConfig.DeadTime = 0;
+	sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+	sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+	sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+	if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN TIM1_Init 2 */
+
+	/* USER CODE END TIM1_Init 2 */
+	HAL_TIM_MspPostInit(&htim1);
+
 }
 
 /**
